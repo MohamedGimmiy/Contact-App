c43a2b646521210a6ba44634d0e34c66ae078e38
Mohamed Hussin <mohamedhussin07@gmail.com>
2022-07-29 17:26:38 +0200

    adding a basic routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contacts', function (){
+    return '<h1> All contacts </h1>';
+});
+Route::get('/contacts/create', function (){
+    return "<h1> Add new Contact </h1>";
+});
+
+Route::get('/contacts/{id}', function ($id){
+    return App\Models\Contact::find($id);
+});
+
+
